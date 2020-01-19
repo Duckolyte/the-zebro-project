52f936378c6fa52f1f3005be23d63f45e70859f8
andygg <andreas.gebert1988@gmail.com>
2020-01-19 23:48:06 +0100

    Initial commit. Project setup done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { AnimalListComponent } from './view/animal-list/animal-list.component';
+import { ZebraProfilComponent } from './view/zebra-profil/zebra-profil.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { ErrorPageComponent } from './page/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimalListComponent,
+    ZebraProfilComponent,
+    DashboardComponent,
+    ErrorPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
