6a86090ea131c997ba9012a808a180f7cf1027d4
Duckolyte <example@example.com>
2020-04-12 18:52:48 +0200

    Minimal header component implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { AnimalListComponent } from './shared/view/animal-list/animal-list.component';
-import { ZebraProfilComponent } from './shared/view/zebra-profil/zebra-profil.component';
-import { DashboardComponent } from './shared/page/dashboard/dashboard.component';
-import { AppRoutingModule } from './shared/app-routing/app-routing.module';
-import { ErrorPageComponent } from './shared/page/error-page/error-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
+import {AnimalListComponent} from './shared/view/animal-list/animal-list.component';
+import {ZebraProfilComponent} from './shared/view/zebra-profil/zebra-profil.component';
+import {DashboardComponent} from './shared/page/dashboard/dashboard.component';
+import {AppRoutingModule} from './shared/app-routing/app-routing.module';
+import {ErrorPageComponent} from './shared/page/error-page/error-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {
   MatCardModule,
   MatDatepickerModule,
 } from '@angular/material';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {RouterStateSerializer, StoreRouterConnectingModule} from '@ngrx/router-store';
 import {CustomSerializer} from './shared/app-store/shared/utils';
-import { reducers, metaReducers } from './shared/app-store';
+import {reducers, metaReducers} from './shared/app-store';
 import {StoreModule} from '@ngrx/store';
 import {EffectsModule} from '@ngrx/effects';
 
-import { MissionsModule } from './missions/missions.module';
-import { ObservationActionsModule } from './observation-actions/observation-actions.module';
-import { AnimalObservationsModule } from './animal-observations/animal-observations.module';
+import {MissionsModule} from './missions/missions.module';
+import {ObservationActionsModule} from './observation-actions/observation-actions.module';
+import {AnimalObservationsModule} from './animal-observations/animal-observations.module';
 import {SharedModule} from './shared/shared.module';
 
 @NgModule({
@@ -54,8 +54,9 @@ import {SharedModule} from './shared/shared.module';
   ],
   providers: [
     MatDatepickerModule,
-    { provide: RouterStateSerializer, useClass: CustomSerializer }
+    {provide: RouterStateSerializer, useClass: CustomSerializer}
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,23 @@ import { CommonModule } from '@angular/common';
 import { StoreModule} from '@ngrx/store';
 
 import {selectionReducer} from './selection-context/selection-context.reducers';
+import { TheHeaderComponent } from './ui-templates/the-header/the-header.component';
+import {MatButtonModule, MatIconModule, MatToolbarModule} from '@angular/material';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TheHeaderComponent],
+  exports: [
+    TheHeaderComponent
+  ],
   imports: [
     CommonModule,
-    StoreModule.forFeature('selections', selectionReducer)
+    StoreModule.forFeature('selections', selectionReducer),
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule
   ]
 })
 
